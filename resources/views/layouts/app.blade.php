d40f567839e9857ba234bae215ed5649b38b627b
mariosath <Athmarios@agenso.gr>
2021-11-03 20:51:03 +0200

    Added instructions and docker support.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Team.blue') }} - @yield('title')</title>
+    <title>{{ config('app.name', 'Team.blue') }} - Todo</title>
 
     <!-- Scripts -->
     <script src="{{ mix('/js/app.js') }}" defer></script>
